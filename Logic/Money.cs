511579b02d4fd6ca00fdef6ca0e68f6d9b5c4995
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2024-06-03 02:20:49 +0400

    Added logic and tests

--- Logic/Money.cs
+++ Logic/Money.cs
@@ -1,13 +1,35 @@
 ﻿namespace Logic;
 
-public class Money
+public class Money : ValueObject<Money>
 {
-    public int OneCentCount { get; private set; }
-    public int TenCentCount { get; private set; }
-    public int QuarterCount { get; private set; }
-    public int OneDollarCount { get; private set; }
-    public int FiveDollarCount { get; private set; }
-    public int TwentyDollarCount { get; private set; }
+    public static readonly Money None = new Money(0, 0, 0, 0, 0, 0);
+    public static readonly Money Cent = new Money(1, 0, 0, 0, 0, 0);
+    public static readonly Money TenCent = new Money(0, 1, 0, 0, 0, 0);
+    public static readonly Money Qaurter = new Money(0, 0, 1, 0, 0, 0);
+    public static readonly Money Dollar = new Money(0, 0, 0, 1, 0, 0);
+    public static readonly Money FiveDollar = new Money(0, 0, 0, 0, 1, 0);
+    public static readonly Money TwentyDollad = new Money(0, 0, 0, 0, 0, 1);
+
+    public int OneCentCount { get;}
+    public int TenCentCount { get;}
+    public int QuarterCount { get;}
+    public int OneDollarCount { get;}
+    public int FiveDollarCount { get;}
+    public int TwentyDollarCount { get;}
+
+    public decimal Amount
+    {
+        get
+        {
+            return OneCentCount * 0.01m +
+                TenCentCount * 0.10m +
+                QuarterCount * 0.25m +
+                OneDollarCount +
+                FiveDollarCount * 5 +
+                TwentyDollarCount * 20;
+        }
+    
+    }
 
     public Money(
          int oneCentCount,
@@ -18,6 +40,19 @@ public class Money
          int twentyDollarCount
         )
     {
+        if (oneCentCount < 0)
+            throw new InvalidOperationException();
+        if (tenCentCount < 0)
+            throw new InvalidOperationException();
+        if (quarterCount < 0)
+            throw new InvalidOperationException();
+        if (oneDollarCount < 0)
+            throw new InvalidOperationException();
+        if (fiveDollarCount < 0)
+            throw new InvalidOperationException();
+        if (twentyDollarCount < 0)
+            throw new InvalidOperationException();
+
         OneCentCount = oneCentCount;
         TenCentCount = tenCentCount;
         QuarterCount = quarterCount;
@@ -26,7 +61,7 @@ public class Money
         TwentyDollarCount = twentyDollarCount;
     }
 
-    public static Money operator +( Money a, Money b )
+    public static Money operator +(Money a, Money b)
     {
         Money sum = new Money(
                 a.OneCentCount + b.OneCentCount,
@@ -38,5 +73,41 @@ public class Money
             );
 
         return sum;
+    }
+
+    public static Money operator -(Money a, Money b)
+    {
+        return new Money(
+                a.OneCentCount - b.OneCentCount,
+                a.TenCentCount - b.TenCentCount,
+                a.QuarterCount - b.QuarterCount,
+                a.OneDollarCount - b.OneDollarCount,
+                a.FiveDollarCount - b.FiveDollarCount,
+                a.TwentyDollarCount - b.TwentyDollarCount
+            );
+    }
+
+    public override bool EqualsCore(Money valueObject)
+    {
+        return OneCentCount == valueObject.OneCentCount &&
+            TenCentCount == valueObject.TenCentCount &&
+            QuarterCount == valueObject.QuarterCount &&
+            OneDollarCount == valueObject.OneDollarCount &&
+            FiveDollarCount == valueObject.FiveDollarCount &&
+            TwentyDollarCount == valueObject.TwentyDollarCount;
+    }
+
+    public override int GetHashCodeCore()
+    {
+        unchecked
+        {
+            int hashCode = OneCentCount;
+            hashCode = (hashCode * 397) ^ TenCentCount;
+            hashCode = (hashCode * 397) ^ QuarterCount;
+            hashCode = (hashCode * 397) ^ OneDollarCount;
+            hashCode = (hashCode * 397) ^ FiveDollarCount;
+            hashCode = (hashCode * 397) ^ TwentyDollarCount;
+            return hashCode;
+        }
     }
 }
--- Logic/SnackMachine.cs
+++ Logic/SnackMachine.cs
@@ -1,24 +1,35 @@
 ﻿namespace Logic;
 
-public class SnackMachine
+public class SnackMachine : Entity
 {
     public Money MoneyInside { get; private set; }
     public Money MoneyInTransaction { get; private set; }
 
+    public SnackMachine()
+    {
+        MoneyInside = Money.None;
+        MoneyInTransaction = Money.None;
+    }
+
     public void InsertMoney(Money money)
     {
+        Money[] coinsAndNotes = { Money.Cent, Money.TenCent, Money.Qaurter, Money.Dollar, Money.FiveDollar, Money.TwentyDollad };
+
+        if (!coinsAndNotes.Contains(money))
+            throw new InvalidOperationException();
+
         MoneyInTransaction += money;
     }
 
     public void ReturnMoney()
     {
-        //MoneyInTransaction = 0;
+        MoneyInTransaction = Money.None;
     }
 
     public void BuySnack()
     {
         MoneyInside += MoneyInTransaction;
 
-        //MoneyInTransaction = 0;
-     }
+        MoneyInTransaction = Money.None;
+    }
 }
